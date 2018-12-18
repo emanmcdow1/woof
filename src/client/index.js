1d9f413a59a70f0a3b2105c1704c92d75ae44972
emanmcdow1 <emanmcdow@gmail.com>
2018-12-18 00:01:13 -0500

    adjusted to work using what we have

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () =>(
+    <div>
+        Hello wirld!!!!!!!!!!!!!!!!!!!!!!!!!!
+    </div>
+)
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
