2879ed734cb8c4f47b7e078201cfe7d67903ed4c
emanmcdow1 <emanmcdow@gmail.com>
2018-12-18 00:18:42 -0500

    added gitignore ;)

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const os = require('os');
 
 const app = express();
-
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/react', function(req,res){
+  app.send('react.png')
+})
 app.listen(8080, () => console.log('Listening on port 8080!'));
